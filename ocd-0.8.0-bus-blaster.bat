6dec499f408285e0adf54e0a98df4be2bae4982e
Lambosaurus <tlamborn@gmail.com>
2015-04-29 16:22:24 +1200

    mmu2

---
+++ ocd-0.8.0-bus-blaster.bat
@@ -0,0 +1,1 @@
+.\tool-chain\openocd-0.8.0\bin\openocd-0.8.0.exe -f .\mmcudemo\mat91lib\sam4s\scripts\sam4s_busblaster.cfg
---
+++ ocd-x64-0.9.0rc1-bus-blaster.bat
@@ -0,0 +1,1 @@
+.\tool-chain\openocd-0.9.0-rc1\bin-x64\openocd.exe -f .\mmcudemo\mat91lib\sam4s\scripts\sam4s_busblaster.cfg
